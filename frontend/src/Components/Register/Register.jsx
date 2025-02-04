1c93af74fd87fd7123bfffadf0e734162da67947
Marccurio <marcnadar03@gmail.com>
2025-02-05 00:56:59 +0530

    added my name

--- frontend/src/Components/Register/Register.jsx
+++ frontend/src/Components/Register/Register.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div className='text-white'>Register</div>
+    <>
+        <div className='text-white'>Register</div>
+        <h1>hello i am marc</h1>
+    </>
   )
 }
 
